a0abfdedac8f6d35edfca8755435dbde3b1e93af
Mauricio <lckcarmine1@hotmail.com>
2025-09-30 13:35:37 -0600

    Write the logic to get the computer choice

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// Function has to return one of the three options randomly
+// Math random returns a number from 0 to 1 (1 is not included)
+// We could create 3 variables and assign a random value and then make a comparison between the three of them to return the computer choice
+
+function getComputerChoice(){
+    let rock = Math.random()
+    let paper = Math.random()
+    let scissors = Math.random()
+
+    if (rock > paper & rock > scissors){
+        return "rock";
+    }else if(paper > rock & paper > scissors){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+    
+}   
+
+console.log(getComputerChoice())
