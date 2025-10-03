bd459c809f100092aa8defb79c8058469acca630
Mauricio <lckcarmine1@hotmail.com>
2025-10-03 13:14:20 -0600

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -4,6 +4,15 @@
 // Math random returns a number from 0 to 1 (1 is not included)
 // We could create 3 variables and assign a random value and then make a comparison between the three of them to return the computer choice
 
+
+function main(){
+    let humanChoice = getHumanChoice()
+    let computerChoice = getComputerChoice()
+    playRound(humanChoice, computerChoice)
+}
+
+
+
 function getComputerChoice(){
     let rock = Math.random()
     let paper = Math.random()
@@ -20,11 +29,26 @@ function getComputerChoice(){
 }   
 
 function getHumanChoice(){
-    let humanChoice = prompt("What's your choice? ")
-    return humanChoice
+    let h_choice = (prompt("What's your choice? ")).toLowerCase()
+    return h_choice
 }
 
-let computerScore = 0
-let humanScore = 0
+function playRound(human,computer){
+    let computerScore = 0
+    let humanScore = 0
+    if ((human == "paper" & computer == "rock") || (human == "rock" & computer == "scissors") || (human == "scissors" & computer == "paper")){
+        humanScore += 1
+        console.log(`You win! ${human} beats ${computer}`)
+        
+    }
+    else if((computer == "paper" & human == "rock") || (computer == "rock" & human == "scissors") || (computer == "scissors" & human == "paper"))
+       {
+         computerScore += 1
+         console.log(`You loose! ${computer} beats ${human}`)
+    }
+    else {
+        console.log("Draw! no points for anyone")
+    }
+}
 
-alert(computerScore)
+main()
